8de4bc02d79560f6f374d1c9f92b8112be83d77b
Richard Beaver <rickybeaver3@gmail.com>
2025-07-04 13:07:57 -0400

    Override Escape to also close floating windows

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,6 +6,26 @@
 
 local map = vim.keymap.set
 
+-- The default LazyVim keymap clears search and stops snippet on escape
+-- Set escape to also close any floating windows
+map({ "i", "n", "s" }, "<esc>", function()
+  vim.cmd("noh")
+  LazyVim.cmp.actions.snippet_stop()
+
+  -- Close any floating windows
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    if vim.api.nvim_win_is_valid(win) then
+      local config = vim.api.nvim_win_get_config(win)
+      if config.relative ~= "" then
+        vim.api.nvim_win_close(win, false)
+      end
+    end
+  end
+
+  -- Send regular <Esc> key
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
+end, { desc = "Escape: Clear hlsearch, stop snippet, close floating windows" })
+
 -- Move to window using the <ctrl> ,aei keys (instead of default hjkl)
 map("n", "<C-,>", "<C-w>h", { desc = "Go to Left Window", remap = true })
 map("n", "<C-a>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
