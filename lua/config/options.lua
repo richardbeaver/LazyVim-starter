84de8a8779f810bfe56160e33f867d3d0dc61830
Richard Beaver <rickybeaver3@gmail.com>
2025-07-04 15:42:10 -0400

    Add other misc options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Visual column at 80 characters
+vim.o.colorcolumn = "80"
+
+-- Keep tabs and indentation of 4 for justfiles
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "just",
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+  end,
+})
