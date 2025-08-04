5a35b0563c8b8e507abecbbd5aa6a13399156f83
Richard Beaver <rickybeaver3@gmail.com>
2025-08-03 21:26:50 -0400

    Add pylint for python files

--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -7,4 +7,20 @@ return {
       },
     },
   },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      -- Event to trigger linters
+      -- events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+      linters_by_ft = {
+        python = { "pylint" },
+        -- fish = { "fish" },
+        -- Use the "*" filetype to run linters on all filetypes.
+        -- ['*'] = { 'global linter' },
+        -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
+        -- ['_'] = { 'fallback linter' },
+        -- ["*"] = { "typos" },
+      },
+    },
+  },
 }
